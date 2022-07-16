489639500082f19cff9fb4252f41580b2b34b966
jigyasu1306 <jigyasu.gautam@lumiq.ai>
2022-07-16 12:44:08 +0530

    changes

--- GCP/vpc/main.tf
+++ GCP/vpc/main.tf
@@ -1,8 +1,12 @@
+resource "google_compute_network" "vpc_network01" {
+  name = var.vpc_network_name
+  auto_create_subnetworks = "false"
+}
 resource "google_compute_subnetwork" "private-subnetwork01" {
   name = var.subnetwork
   ip_cidr_range = "10.2.0.0/16"
   region = var.region
-  network = var.vpc_network_name
+  network = google_compute_network.vpc_network01
   private_ip_google_access = "false"
 }
 
@@ -15,10 +19,6 @@ resource "google_compute_subnetwork" "private-subnetwork02" {
   name = var.subnetwork02
   ip_cidr_range = "10.3.0.0/16"
   region = var.region
-  network = var.vpc_network_name02
+  network = google_compute_network.vpc_network_name02
   private_ip_google_access = "true"
-}
-resource "google_compute_network" "vpc_network02" {
-  name = var.vpc_network_name02
-  auto_create_subnetworks = "false"
 }
