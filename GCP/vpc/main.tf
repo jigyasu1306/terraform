832f0dd39ff3f034d29c319c0cdcaa3227f75f7e
jigyasu1306 <jigyasu.gautam@lumiq.ai>
2022-07-16 16:35:10 +0530

    msgf

--- GCP/vpc/main.tf
+++ GCP/vpc/main.tf
@@ -7,5 +7,5 @@ resource "google_compute_subnetwork" "public-subnetwork" {
 name = var.subnetwork
 ip_cidr_range = "10.2.0.0/16"
 region = var.region
-network = google_compute_network.vpc_network.name
+network = google_compute_network.vpc-network.name
 }
