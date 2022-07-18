20678fa38ad47871484c4f110f57b995bc043c3b
jigyasu1306 <jigyasu.gautam@lumiq.ai>
2022-07-18 12:33:53 +0530

    changes done

--- GCP/vpc-connector/main.tf
+++ GCP/vpc-connector/main.tf
@@ -34,7 +34,7 @@ resource "google_compute_router" "router" {
 resource "google_compute_router_nat" "nat" {
   name = var.nat
   provider = google-beta
-  region = "us-west1"
+  region = var.region
   router = google_compute_router.router.name
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
   nat_ip_allocate_option = "AUTO_ONLY"
