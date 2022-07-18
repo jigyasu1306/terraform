4f4b03ae8f0c65f66085d479a4a347cfb5722463
jigyasu1306 <jigyasu.gautam@lumiq.ai>
2022-07-18 10:55:03 +0530

    msgk

--- GCP/Compute_engine/main.tf
+++ GCP/Compute_engine/main.tf
@@ -1,7 +1,15 @@
-resource "google_compute_network" "vpc_network" {
+resource "google_compute_network" "vpc-network" {
   name = var.vpc_network_name
-  auto_create_subnetworks = "true"
+  auto_create_subnetworks = "false"
 }
+
+resource "google_compute_subnetwork" "public-subnetwork" {
+name = var.subnetwork
+ip_cidr_range = "10.2.0.0/16"
+region = var.region
+network = google_compute_network.vpc-network.name
+}
+
 
 resource "google_compute_instance" "vm_instance" {
   name = "new-terraform instance"
