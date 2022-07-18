f05003208432fd2317fc804c33a9516893412ab3
jigyasu1306 <jigyasu.gautam@lumiq.ai>
2022-07-18 15:19:34 +0530

    changes done

--- GCP/PSCEP/variable.tf
+++ GCP/PSCEP/variable.tf
@@ -14,17 +14,17 @@ variable "firewall" {
 }
 
 variable "nat_name" {
-    type = "string"
+    type = string
     description = "Name of NAT gateway"
 }
 
 variable "router_name" {
-    type = "string"
+    type = string
     description = "Name of Router"
 }
 
 variable "forwarding_rule" {
-    type = "string"
+    type = string
     description = "Name of firewall_rule"
 }
 
