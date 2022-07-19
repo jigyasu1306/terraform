66875dee23856b3d39a7e25919786cf72cbed069
jigyasu1306 <jigyasu.gautam@lumiq.ai>
2022-07-19 10:16:50 +0530

    changes done

--- GCP/PSCEP/main.tf
+++ GCP/PSCEP/main.tf
@@ -1,66 +1,78 @@
 resource "google_compute_service_attachment" "psc_ilb_service_attachment" {
-  name = var.psc_ilb
-  region = var.region
-  //domain_names = ["gcp.tfacc.hashicorptest.com."]
-  enable_proxy_protocol = true
-  connection_preference = "ACCEPT_AUTOMATIC"
-  nat_subnets = [google_compute_subnetwork.psc_ilb_nat.id]
-  target_service = google_compute_forwarding_rule.psc_ilb_target_service.id
+  name        = "my-psc-ilb"
+  region      = var.region
+  description = "A service attachment configured with Terraform"
+
+  domain_names             = ["gcp.tfacc.hashicorptest.com."]
+  enable_proxy_protocol    = true
+  connection_preference    = "ACCEPT_AUTOMATIC"
+  nat_subnets              = [google_compute_subnetwork.psc_ilb_nat.id]
+  target_service           = google_compute_forwarding_rule.psc_ilb_target_service.id
 }
 
 resource "google_compute_address" "psc_ilb_consumer_address" {
-  name = "psc-ilb-consumer-address"
+  name   = "psc-ilb-consumer-address"
   region = var.region
-  subnetwork = var.subnetwork-name
+  subnetwork   = "default"
   address_type = "INTERNAL"
 }
 
 resource "google_compute_forwarding_rule" "psc_ilb_consumer" {
-  name   = var.forwarding_rule
+  name   = "psc-ilb-consumer-forwarding-rule"
   region = var.region
-  target = google_compute_service_attachment.psc_ilb_service_attachment.name
+
+  target                = google_compute_service_attachment.psc_ilb_service_attachment.id
   load_balancing_scheme = "" # need to override EXTERNAL default when target is a service attachment
-  network = "default"
-  ip_address = google_compute_address.psc_ilb_consumer_address.id
+  network               = "default"
+  ip_address            = google_compute_address.psc_ilb_consumer_address.id
 }
 
 resource "google_compute_forwarding_rule" "psc_ilb_target_service" {
   name   = "producer-forwarding-rule"
   region = var.region
+
   load_balancing_scheme = "INTERNAL"
-  backend_service = google_compute_region_backend_service.producer_service_backend.id
-  all_ports = true
-  network = google_compute_network.psc_ilb_network.name
-  subnetwork = google_compute_subnetwork.psc_ilb_producer_subnetwork.name
+  backend_service       = google_compute_region_backend_service.producer_service_backend.id
+  all_ports             = true
+  network               = google_compute_network.psc_ilb_network.name
+  subnetwork            = google_compute_subnetwork.psc_ilb_producer_subnetwork.name
 }
 
 resource "google_compute_region_backend_service" "producer_service_backend" {
   name   = "producer-service"
   region = var.region
+
   health_checks = [google_compute_health_check.producer_service_health_check.id]
 }
+
 resource "google_compute_health_check" "producer_service_health_check" {
   name = "producer-service-health-check"
+
   check_interval_sec = 1
   timeout_sec        = 1
   tcp_health_check {
     port = "80"
   }
 }
+
 resource "google_compute_network" "psc_ilb_network" {
   name = "psc-ilb-network"
   auto_create_subnetworks = false
 }
+
 resource "google_compute_subnetwork" "psc_ilb_producer_subnetwork" {
-  name = "psc-ilb-producer-subnetwork"
+  name   = "psc-ilb-producer-subnetwork"
   region = var.region
-  network = google_compute_network.psc_ilb_network.id
+
+  network       = google_compute_network.psc_ilb_network.id
   ip_cidr_range = "10.0.0.0/16"
 }
+
 resource "google_compute_subnetwork" "psc_ilb_nat" {
   name   = "psc-ilb-nat"
   region = var.region
-  network = google_compute_network.psc_ilb_network.id
-  purpose =  "PRIVATE_SERVICE_CONNECT"
+
+  network       = google_compute_network.psc_ilb_network.id
+  purpose       =  "PRIVATE_SERVICE_CONNECT"
   ip_cidr_range = "10.1.0.0/16"
 }
